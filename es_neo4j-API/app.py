b372583d78276ce29d5baf582847688b043941f9
thanos.batsiou <thanos.batsiou@gmail.com>
2023-12-05 17:47:04 +0200

    Initial commit for es_neo4j api

---
+++ es_neo4j-API/app.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:100f171869be87610a24a80208ffbd2087281df198e26b012750fd9ba01b031a
+size 228
---
+++ es_neo4j-API/routes/documents.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:98e4dc6fd23ff2880a8842657834d8de773a1eda4d852268d0c3daa1828eec03
+size 214
