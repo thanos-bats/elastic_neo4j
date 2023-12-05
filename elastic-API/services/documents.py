32010ee1c5a29266ef97cc7d94ae6929813b8ae4
thanos.batsiou <thanos.batsiou@gmail.com>
2023-12-05 17:25:09 +0200

    Complete documents service

--- elastic-API/services/documents.py
+++ elastic-API/services/documents.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:37f4f8b6d3f806c32fc0d3e19cb8452e59a5c4ebcb670997828f059b98a4e5e5
-size 524
+oid sha256:394ab349d9cf29b97b307925b929cfc8ccb0620bc3084fcf492b6d6a29130032
+size 2374
