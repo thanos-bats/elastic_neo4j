8874d88cc193bfb15acd7015403237fa4d77de57
thanos.batsiou <thanos.batsiou@gmail.com>
2023-12-07 13:40:44 +0200

    Update the mapping setting

--- elastic-API/files/main.py
+++ elastic-API/files/main.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b1d304469c5faa8f97de0d07428dd9b3879d0637f486804a19dc2662b0800e41
-size 2600
+oid sha256:7f414393d6cd764509dfe14d8b98ba77df2159bf34b8b2c51727d5b9bdc5ba8a
+size 4738
