4eda8cc4e687c6abdcdffd1f5b9a339a078a0766
thanos.batsiou <thanos.batsiou@gmail.com>
2023-12-07 13:39:37 +0200

    Add  combine and clean list on utils file

--- elastic-API/utilities/utils.py
+++ elastic-API/utilities/utils.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5c9bad2fa72a55c35689326a0879165abbda28a50240d740c163897116e3c861
-size 856
+oid sha256:03fae53cf2a1637b37536866329ccd6b20dccde4db60e699653ad1b131a0e7c8
+size 1445
