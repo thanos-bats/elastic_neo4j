58cb57cc06e0686153fadfc13911d9ab783b6b77
thanos.batsiou <thanos.batsiou@gmail.com>
2023-12-07 13:38:16 +0200

    New endpoint to search by regex

--- elastic-API/routes/documents.py
+++ elastic-API/routes/documents.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:eef07cb83b4b59995a504ce6cea0815b75a41b6381c8b6698c4289b59b7f086f
-size 3141
+oid sha256:f5af5c1949928e4a217ef5e0db3440dea28d9d8af000acc7517992a9afe267af
+size 5174
