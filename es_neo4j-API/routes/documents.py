dcf8c8cc886846dd54191a0c7191b91944cf4baf
thanos.batsiou <thanos.batsiou@gmail.com>
2023-12-11 12:57:54 +0200

    Hyper get documents

--- es_neo4j-API/routes/documents.py
+++ es_neo4j-API/routes/documents.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:98e4dc6fd23ff2880a8842657834d8de773a1eda4d852268d0c3daa1828eec03
-size 214
+oid sha256:3f6950239f9ef3d37f1307cdcb1afe66d53911108cd10da77d6f580eab9d5914
+size 4271
