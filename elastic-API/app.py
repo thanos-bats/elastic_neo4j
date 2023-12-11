d879cedd725d92f591263a2421fdfa9d13ca78f1
thanos.batsiou <thanos.batsiou@gmail.com>
2023-12-11 12:59:26 +0200

    Disable urllib3 warnings

--- elastic-API/app.py
+++ elastic-API/app.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:061092b53facc0477f5a3b0f16b481cb5d0c5f5a6d83b04befa43cd01eda3287
-size 228
+oid sha256:bd6806b0cc9e645c44e6430b002471e108d0f43b804bc0dc73d9c223834e927f
+size 312
