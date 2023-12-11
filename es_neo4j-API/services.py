4fd5be96b0df0edc82a9f5506a46ff657bca8455
thanos.batsiou <thanos.batsiou@gmail.com>
2023-12-11 12:58:18 +0200

    Paginate ids function

--- es_neo4j-API/services.py
+++ es_neo4j-API/services.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f0e22d146bca6afcc30cfef8d2913cda6aaee47f6cadb2f26a77d24bd2981b1c
-size 265
+oid sha256:5c2e2e2c6038f7ee5b0c794f12a88ecd6b74c1b42313ef95c3920ef8e9a9d37c
+size 720
