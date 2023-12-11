7b626c239eacace3c5f29c29a89253b015db2ba5
thanos.batsiou <thanos.batsiou@gmail.com>
2023-12-11 12:58:47 +0200

    Minor changes

--- elastic-API/services/documents.py
+++ elastic-API/services/documents.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1d4aa91a1caadabb83170cf2e09745f89fd8b66a5b82b92fc30a9465cc8e88c1
-size 3385
+oid sha256:38a66cfb30758907fb79904e9afa672762db3c8d9fece8873227845f9898c731
+size 3182
